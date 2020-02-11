3a0099ee9e7a74cc9e3c7ceb7a4ac9289c6d5610
Raf Sobol <rafal.sobol@academytrainee.com>
2020-02-11 10:06:33 +0000

    Added ability to send in database password via maven.

--- src/main/java/com/qa/sobolinventory/Connector.java
+++ src/main/java/com/qa/sobolinventory/Connector.java
@@ -1,16 +1,30 @@
 package com.qa.sobolinventory;
 
 import java.sql.Connection;
+import java.io.File;
+import java.net.URL;
+
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Connector {
 	private Connection connection;
+	private String password;
+	private static final String CONFIG_FILE = "/app.properties";
+	public File readFileFromClasspath() {
+        URL fileUrl = getClass().getResource(CONFIG_FILE);
+        return new File(fileUrl.getFile());
+
+	}
+	
 	public Connector(String url) throws SQLException {
+		password = System.getenv("env.PWD");
+		if (password == "") {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Password: ");
-		String password = scanner.nextLine();
+		password = scanner.nextLine();
+		}
 		try {
 			this.connection = DriverManager.getConnection(url, "root",
 					password);
@@ -18,7 +32,6 @@ public class Connector {
 			System.out.println(e.getMessage());
 			
 		}
-		scanner.close();
 
 	}
 	
